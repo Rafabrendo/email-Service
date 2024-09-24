53baef440dd872f9c53650d61c2a936977c497c5
rafabrendo <rafalive122@hotmail.com>
2024-09-24 02:52:15 -0300

    adding style

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,64 @@
-import { SetStateAction, useState } from 'react'
-import './App.css'
-import PrimaryInput from './components/Input/PrimaryInputs'
+import { useState } from 'react';
+import './App.css';
+import { PrimaryInput} from './components/Input/PrimaryInputs';
+import { Button, Spacer } from '@chakra-ui/react';
+
 
 function App() {
-  const [email, setEmail] = useState('')
-  
+  const [formData, setFormData] = useState({
+    email: '',
+    firstName: '',
+    secondName: ''
+  });
 
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
 
   return (
+    
     <div className='container'>
+      <form>
 
-      <PrimaryInput 
-        value={email} 
-        onChange={event => setEmail(event.target.value)} 
+      <div className='name-form-container'>
+        <Spacer height="4" />
+        <PrimaryInput
+          value={formData.firstName}
+          onChange={handleChange}
+          name="firstName"
+          label="Nome"
+          placeholder="Digite seu nome"
+        />
+
+        <PrimaryInput
+          value={formData.secondName}
+          onChange={handleChange}
+          name="secondName"
+          label="Sobrenome"
+          placeholder='Digite seu Sobrenome'
+        />
+      </div>
+      <PrimaryInput
+        value={formData.email}
+        onChange={handleChange}
         name="email"
-        label='Digite seu e-mail' 
+        label="E-mail"
+        placeholder='Digite seu email'
       />
+      <Spacer height="4" />
+      <Button colorScheme='green'>Enviar</Button>
+
+      </form>
+
+      <div className="product-details">
+        
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Input/PrimaryInputs.tsx
+++ src/components/Input/PrimaryInputs.tsx
@@ -2,20 +2,23 @@ import {Input} from "@chakra-ui/react";
 import { ChangeEventHandler } from "react";
 import "./primary-input.css"
 
+
 interface PrimaryInputProps{
     name:string,
     value: string,
-    onChange: ChangeEventHandler<HTMLInputElement>
-    label: string
+    onChange: ChangeEventHandler<HTMLInputElement>,
+    label: string,
+    placeholder:string
+
 }
 
-export default function PrimaryInput({name, value, onChange, label}: PrimaryInputProps){
+export function PrimaryInput({name, value, onChange, label, placeholder}: PrimaryInputProps){
     return(
         <div className="input-container">  
             <label className="label">{label}</label>
             <Input 
                 variant='filled' 
-                placeholder='Writer your email' 
+                placeholder={placeholder}
                 name={name} 
                 value={value} 
                 onChange={onChange}
@@ -25,3 +28,39 @@ export default function PrimaryInput({name, value, onChange, label}: PrimaryInpu
         
     )
 }
+
+// export function PrimaryInputName({name, value, onChange, label}: PrimaryInputProps)
+// {
+//     return (
+//         <div className="input-container">
+//             <label className="label">{label}</label>
+//             <Input 
+//                 variant='filled' 
+//                 placeholder='Writer your name' 
+//                 name={name} 
+//                 value={value} 
+//                 onChange={onChange}
+                
+//             />
+//         </div>
+//     )
+// }
+
+// export function PrimaryInputLastName({name, value, onChange, label}: PrimaryInputProps)
+// {
+//     return (
+//         <div className="input-container">
+//             <label className="label">{label}</label>
+//             <Input 
+//                 variant='filled' 
+//                 placeholder='Writer your last name' 
+//                 name={name} 
+//                 value={value} 
+//                 onChange={onChange}
+                
+//             />
+//         </div>
+//     )
+// }
+
+
